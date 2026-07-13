3a516632298687da5c888da1159fa1a040f2fabf
sakthi5856k-cell <sakthi5856k@gmail.com>
2026-07-13 10:04:07 +0530

    Update contact.js

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,9 +1,27 @@
 import express from "express";
+import Contact from "../models/Contact.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", async(req,res)=>{
 
+const contact = await Contact.create(req.body);
+
+res.json(contact);
+
+});
+
+router.get("/", async(req,res)=>{
+
+const contacts = await Contact.find().sort({
+createdAt:-1
+});
+
+res.json(contacts);
+
+});
+router.post("/", async (req, res) => {
+  
   res.json({
     success: true,
     message: "Message Received"
