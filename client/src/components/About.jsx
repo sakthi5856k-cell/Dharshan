28dee0a0075a2d4fd40182734ecc84e64af3f5fa
sakthi5856k-cell <sakthi5856k@gmail.com>
2026-07-13 08:56:15 +0530

    Create About.jsx

---
+++ client/src/components/About.jsx
@@ -0,0 +1,36 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function About(){
+
+return(
+
+<>
+
+<Navbar/>
+
+<section className="py-24 max-w-6xl mx-auto">
+
+<h1 className="text-5xl font-bold">
+
+About Us
+
+</h1>
+
+<p className="mt-8 text-lg">
+
+DHARSHAN Packers & Movers is powered by Gayatri Transport.
+
+We provide safe, reliable and affordable moving solutions across India.
+
+</p>
+
+</section>
+
+<Footer/>
+
+</>
+
+)
+
+}
